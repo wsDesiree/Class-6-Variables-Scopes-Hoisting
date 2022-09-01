ff6eb1e539ee11c9bd0d6bdad9aed3249bd8ed9f
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-09-01 17:44:21 -0500

    Class-6-exercises

---
+++ warmup-exercise.js
@@ -0,0 +1,15 @@
+// const num= 1
+// function countTo(num){
+//     let (num > 5, );{
+
+//     }
+// }
+// console.log(5);
+
+
+function countTo(num){
+    for(let i = 0; i <= num;i++){
+        console.log(i)
+    }
+}
+countTo(5);
